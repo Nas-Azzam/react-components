ca257e3ac31158ba3ca37e4b42fcf1f057f33855
XDzerO7 <wubih1122@gmail.com>
2021-10-23 16:41:46 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import setup from './tutorial/1-useState/setup/1-error-example'
-// import setup from './tutorial/1-useState/setup/2-useState-basics'
-// import setup from './tutorial/1-useState/setup/3-useState-array
+import setup from './tutorial/1-useState/setup/3-useState-array.js'
 
 function App() {
   return (
     <div className='container'>
-      <h2><setup /
+      <h2>
+        <setup />
       </h2>
     </div>
   )
--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import { data } from '../../../data';
+import React from 'react'
+import { data } from '../../../data'
 
 const UseStateArray = () => {
-const [perople , setpeople] = React.useState(data);
-  return( 
-  <>
-  {perople.map((person) =>{
-    const{id,name} = person
-    return 'helloooo meo meo'
-    return <div key = {id} className = 'item' > 
-    <h3>{{name}</h3>
-       
-    </div>
-    
- 
-  })
-  }
-  <button className = 'btn' onClick = {()=>setpeople([])}  >clear items</button>
-  </>
-};
+  const [perople, setpeople] = React.useState(data)
+  return (
+    <>
+      {perople.map((person) => {
+        const { id, name } = person
 
-export default UseStateArray;
+        return (
+          <div key={id} className='item'>
+            <h3>{name}</h3>
+          </div>
+        )
+      })}
+      <button className='btn' onClick={() => setpeople([])}>
+        clear items
+      </button>
+    </>
+  )
+}
+
+export default UseStateArray
